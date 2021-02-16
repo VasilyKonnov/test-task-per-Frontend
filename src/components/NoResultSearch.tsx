b8ddd05b19a77ae0e67c531c5c214c5504e17f41
VASILY-KONNOV <vasiliy.konnov@gmail.com>
2021-02-16 08:17:27 +0400

    Updates

--- src/components/NoResultSearch.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react"
-
-const NoResultSearch = () => {
-	return (
-		<h1 style={{ marginTop: "50px" }}>Ни чего не нашлось, попробуйте другой запрос...</h1>
-	)
-}
-
-export default NoResultSearch;
--- src/constants.tsx
+++
@@ -1,4 +0,0 @@
-export const URL_LEAGUES = "https://api.football-data.org/v2/competitions"
-export const URL_COMANDS = "https://api.football-data.org/v2/teams"
-export const API_TOKEN = "0d5df8c05978490ea0958129ea7986b4"
-export const TARIF = "?plan=TIER_ONE"
